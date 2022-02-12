a2eddc53078a2f748338085c0990dd9bc24414d2
badrinath117 <70693954+badrinath117@users.noreply.github.com>
2022-02-12 21:46:17 +0530

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,15 +10,16 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          {emoji("Designed by - Badrinath")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
+          <a href="https://github.com/badrinath117">
+            Badrinath
           </a>
         </p>
       </div>
     </Fade>
   );
 }
+
